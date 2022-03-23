07631e4f92a6cb5f25aeee78f4b273d84529325c
Gladyskyenze <kyenzegladys@gmail.com>
2022-03-24 00:58:17 +0300

    made the contact page responsive

---
+++ jscript/conta.js
@@ -0,0 +1,7 @@
+$('#user_form').submit(function () {
+    let namee = $('#name').val();
+    let emaill = $('#email').val();
+    let msg = $('#messagee').val();
+    alert(namee + 'We have received your message. Thank you for the feedback.');
+
+});
